0d1646404cc172ab27a7b646d167eb8259c2dc1d
User 3 <segdevops@lntinfotech.com>
2018-04-02 19:07:43 +0530

    first commit

---
+++ src/main/java/SikuliTest/sikuli/App.java
@@ -0,0 +1,13 @@
+package SikuliTest.sikuli;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
