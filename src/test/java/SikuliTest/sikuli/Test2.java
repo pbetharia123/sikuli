44303c47a9027f70ab32bc97c99eb65accf8a8be
User 3 <segdevops@lntinfotech.com>
2018-04-06 11:36:17 +0530

    updated pom with monte

--- src/test/java/SikuliTest/sikuli/Test2.java
+++ src/test/java/SikuliTest/sikuli/Test2.java
@@ -3,18 +3,9 @@ package SikuliTest.sikuli;
 public class Test2 {
 	public static void main(String[] args) {
 		  
-	     int n = -5;    
-	     if (HelperClass.isValidInteger(n)) 
-	         System.out.println("True");
-	     else
-	         System.out.println("False");
-	      
-	  
-	     String str = "madam";    
-	     if (HelperClass.isPalindrome(str)) 
-	        System.out.println("True");
-	     else
-	        System.out.println("False");        
+	    String path = System.getProperty("user.dir");
+	    System.out.println("current directory is "+path);
+	    
 	 }
 
 }
