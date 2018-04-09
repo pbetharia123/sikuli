b368dfbe1dea66e910af5a71ca8757a7b2097b7e
User 3 <segdevops@lntinfotech.com>
2018-04-09 16:06:44 +0530

    class file updated

--- src/test/java/SikuliTest/sikuli/launchTab.java
+++ src/test/java/SikuliTest/sikuli/launchTab.java
@@ -21,10 +21,12 @@ public class launchTab {
 		
 	}
 	
+	
 	public static void opentab() throws Exception
 	{
 		
 		screenRecorder src = new screenRecorder();
+		src.startRecord();
 		
 		Screen s = new Screen();
 		//s.click("img/tablaue_desktop_icon.png");
@@ -45,7 +47,11 @@ public class launchTab {
 		Date startdt = new Date();
 		s.wait("img/record_count.png",500);
 		Date enddt = new Date();
+		if (s.exists("img/record_count.png")!=null){
+			System.out.println("The data is loaded in MySQL successfully");
+		}
 		
+		src.stopRecord();
 		long diff = enddt.getTime() - startdt.getTime();
 		System.out.println("Time taken is "+diff/1000);
 		//src.stopRec();
@@ -66,6 +72,7 @@ public class launchTab {
 		
 		s.click("img/mysql.png");
 		
+		s.wait("img/mysqlservername.png",1500);
 		s.click("img/mysqlservername.png");
 		s.type("a", KeyModifier.CTRL);
 		s.type(Key.BACKSPACE);
